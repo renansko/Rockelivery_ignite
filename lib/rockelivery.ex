f6438082660aeb46c36cb506ea4c8a4c2422700c
renansko <renansko@gmail.com>
2021-07-05 15:50:45 -0300

    first commit

---
+++ lib/rockelivery.ex
@@ -0,0 +1,7 @@
+defmodule Rockelivery do
+
+  alias Rockelivery.Users.Create, as: UserCreate
+
+defdelegate create_user(params), to: UserCreate, as: :call
+
+end
---
+++ lib/rockelivery/user.ex
@@ -0,0 +1,42 @@
+defmodule Rockelivery.User do
+use Ecto.Schema
+import Ecto.Changeset
+
+alias Ecto.Changeset
+@primary_key {:id, :binary_id, autogenerate: true}
+
+@fields [:age, :address, :cep, :cpf, :email, :password, :name]
+
+@derive {Jason.Encoder, only: [:id, :age, :cpf,:address, :email]}
+schema "users" do
+    field :age, :integer
+    field :address, :string
+    field :cep, :string
+    field :cpf, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @fields)
+    |> validate_required(@fields)
+    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:cep, min: 8)
+    |> validate_length(:cpf, min: 11)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> unique_constraint(:email)
+    |> unique_constraint(:cpf)
+    |> validate_format(:email, ~r/@/)
+    |> put_password_hash()
+  end
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Pbkdf2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
