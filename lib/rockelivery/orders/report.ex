e1641c437bb8282fe3c0d26cd397e4b18438fa9e
Renan Skonicezny Vilela <renan.vilela@teros.com.br>
2023-04-19 17:01:51 -0300

    Reajust create Items and start report.ex in orders

--- lib/rockelivery/orders/report.ex
+++ lib/rockelivery/orders/report.ex
@@ -1,5 +1,15 @@
 defmodule Rockelivery.Orders.Report do
-  def create do
-    Repo.all()
+  import Ecto.Query
+  alias Rockelivery.{Repo, Order}
+
+  def create(filename \\ "report.csv") do
+    query = from order in Order, order_by: order.user_id
+
+    Repo.transaction(fn ->
+      query
+      |> Repo.stream(max_rows: 4)
+      |> Enum.map(fn item -> item end)
+      |> IO.inspect()
+    end)
   end
 end
