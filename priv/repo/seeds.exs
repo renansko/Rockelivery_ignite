8028b03b9d6d3d3b162264a1879c41568f27c516
Renan Skonicezny Vilela <renan.vilela@teros.com.br>
2023-04-29 00:42:40 -0300

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,44 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rockelivery.{Item, Order, Repo}
+
+user = %User{
+  age: 27,
+  address: "Rua das alameidas 203",
+  cep: "123456",
+  cpf: "78965432158"
+  email: "tenebroso@cabuloso.com",
+  password: "123456",
+  name: "Jorge"
+}
+
+%User{id: user_id} = Repo.insert!(user)
+
+item1= %Item{
+  category: :food,
+  description: "Banana",
+  price: Decimal.new("15.05"),
+  photo: "priv/photos/banana_cozida;png"
+}
+
+item2= %Item{
+  category: :food,
+  description: "Coca",
+  price: Decimal.new("11.05"),
+  photo: "priv/photos/coquita;png"
+}
+
+Repo.insert!(item1)
+Repo.insert!(item2)
+
+order = %Order{
+  user_id: user_id,
+  items: [item1, item2, item3],
+  address: "Rua das alameidas",
+  comments: "Sem canela",
+  payment_method: :money
+}
+
+Repo.insert!(order)
